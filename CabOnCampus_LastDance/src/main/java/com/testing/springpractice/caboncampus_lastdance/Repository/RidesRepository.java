d6429260945a0291c0e4e7580d81ada069ab7bd7
Harnimarpreet Singh Bindra <hnps.bindra@gmail.com>
2026-07-21 23:32:18 +0530

    Update RidesRepository.java

--- CabOnCampus_LastDance/src/main/java/com/testing/springpractice/caboncampus_lastdance/Repository/RidesRepository.java
+++ CabOnCampus_LastDance/src/main/java/com/testing/springpractice/caboncampus_lastdance/Repository/RidesRepository.java
@@ -1,108 +1,133 @@
 package com.testing.springpractice.caboncampus_lastdance.Repository;
 
-import com.testing.springpractice.caboncampus_lastdance.DTO.*;
+import com.testing.springpractice.caboncampus_lastdance.DTO.RideDetails;
 import com.testing.springpractice.caboncampus_lastdance.Models.RideStatus;
-import com.testing.springpractice.caboncampus_lastdance.Models.Role;
+import com.testing.springpractice.caboncampus_lastdance.Models.Rides;
 import com.testing.springpractice.caboncampus_lastdance.Models.Users;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, UUID> {
-   Users findByUsername(String username);
+public interface RidesRepository extends JpaRepository<Rides, UUID> {
+    @Query("select new com.testing.springpractice.caboncampus_lastdance.DTO.RideDetails(" +
+            "r.id," +
+            "u.id," +
+            "r.pickup," +
+            "r.drop," +
+            "u.name," +
+            "u.phoneNumber," +
+            "u.vehicleNumber," +
+            "r.status) " +
+            "from Rides r " +
+            "left JOIN r.driver u " +
+            "where r.status = :status " +
+            "And r.student.username=:username " +
+            "order by r.createdAt desc")
+    List<RideDetails> getAllRidesByRideStatus(RideStatus status, String username);
 
-   boolean existsByUsername(String username);
+    @Query("select new com.testing.springpractice.caboncampus_lastdance.DTO.RideDetails(" +
+            "r.id," +
+            "u.id," +
+            "r.pickup," +
+            "r.drop," +
+            "u.name," +
+            "u.phoneNumber," +
+            "u.vehicleNumber," +
+            "r.status) " +
+            "from Rides r " +
+            "left JOIN r.driver u " +
+            "where " +
+            "r.student.username=:username " +
+            "order by r.createdAt desc")
+    List<RideDetails> getAllRides( String username);
 
+    @Query("select new com.testing.springpractice.caboncampus_lastdance.DTO.RideDetails(" +
+            "r.id," +
+            "u.id," +
+            "r.pickup," +
+            "r.drop," +
+            "u.name," +
+            "u.phoneNumber," +
+            "u.vehicleNumber," +
+            "r.status) " +
+            "from Rides r " +
+            "left JOIN r.driver u " +
+            "where r.id = :rideId")
+    RideDetails getRideById(UUID rideId);
 
+    @Modifying
+    @Transactional
+    @Query("""
+    UPDATE Rides r
+    SET r.status = :status
+    WHERE r.id = :rideId
+""")
+    int updateRideStatus(UUID rideId, RideStatus status);
 
-   @Modifying
-   @Transactional
-   @Query("Update Users u " +
-           "set u.password = :password " +
-           "where u.username = :username")
-   int updatepasswordForusername(String password, String username);
+    @Query("select new com.testing.springpractice.caboncampus_lastdance.DTO.RideDetails(" +
+            "r.id," +
+            "u.id," +
+            "r.pickup," +
+            "r.drop," +
+            "u.name," +
+            "u.phoneNumber," +
+            "u.rollNumber," +
+            "r.status) " +
+            "from Rides r " +
+            "left JOIN r.student u " +
+            "where r.status=:status")
+    List<RideDetails> getRideByStatus(RideStatus status);
 
-   @Query("select new  com.testing.springpractice.caboncampus_lastdance.DTO.ProfileResponse " +
-           "(u.id," +
-           "u.name," +
-           "u.username," +
-           "u.phoneNumber," +
-           "u.rollNumber) " +
-           "From Users u " +
-           "WHERE u.username=:username")
-   ProfileResponse selectFields(String username);
+    @Transactional
+    @Modifying
+    @Query("update Rides r " +
+            "set r.driver = :user, " +
+            "r.status = :status " +
+            "where r.id = :rideId and r.status = :oldStatus and r.driver is null"
+    )
+    int acceptARide(Users user, UUID rideId, RideStatus status, RideStatus oldStatus);
 
-   @Transactional
-   @Modifying
-   @Query("UPDATE Users u " +
-           "set u.phoneNumber= :phoneNumber," +
-           "u.rollNumber = :rollNumber " +
-           "where u.username = :username")
-   int updateFields(String phoneNumber, String rollNumber, String username);
+    @Query("select new com.testing.springpractice.caboncampus_lastdance.DTO.RideDetails(" +
+            "r.id," +
+            "u.id," +
+            "r.pickup," +
+            "r.drop," +
+            "u.name," +
+            "u.phoneNumber," +
+            "u.rollNumber," +
+            "r.status) " +
+            "from Rides r " +
+            "left JOIN r.student u " +
+            "where " +
+            "r.driver.username=:username " +
+            "order by r.createdAt desc")
+    List<RideDetails> getAllDriverRides( String username);
 
-   @Query("select new com.testing.springpractice.caboncampus_lastdance.DTO.DriverDetails(" +
-           "u.id," +
-           "u.name," +
-           "u.phoneNumber," +
-           "u.vehicleNumber) " +
-           "from Users u where u.role = :role" )
-   List<DriverDetails> getAllUserWithRole(Role role);
+    @Query("select new com.testing.springpractice.caboncampus_lastdance.DTO.RideDetails(" +
+            "r.id," +
+            "u.id," +
+            "r.pickup," +
+            "r.drop," +
+            "u.name," +
+            "u.phoneNumber," +
+            "u.rollNumber," +
+            "r.status) " +
+            "from Rides r " +
+            "left JOIN r.student u " +
+            "where " +
+            "r.driver.username=:username AND r.id=:rideId " +
+            "order by r.createdAt desc")
+    RideDetails getDriverRide(UUID rideId, String username);
 
-
-   @Query("select new com.testing.springpractice.caboncampus_lastdance.DTO.UserDescription(" +
-           "u.id," +
-           "u.name," +
-           "u.username," +
-           "u.phoneNumber," +
-           "u.role," +
-           "u.vehicleNumber," +
-           "u.rollNumber) " +
-           "from Users u where u.role=:role")
-   Page<UserDescription> getUsersAdminView(Role role, Pageable pageable);
-
-   @Transactional
-   @Modifying
-   @Query("update Users u " +
-           "set u.enabled=false " +
-           "where u.id = :userId")
-   int suspendUserById(UUID userId);
-
-   @Transactional
-   @Modifying
-   @Query("update Users u " +
-           "set u.enabled=true " +
-           "where u.id = :userId")
-   int unsuspendUserById(UUID userId);
-
-
-   @Query("select new  com.testing.springpractice.caboncampus_lastdance.DTO.DriverProfileDetails " +
-           "(u.id," +
-           "u.name," +
-           "u.username," +
-           "u.phoneNumber," +
-           "u.vehicleNumber) " +
-           "From Users u " +
-           "WHERE u.username=:username")
-   DriverProfileDetails getDriverProfileDetailsByUsername(String username);
-
-   @Transactional
-   @Modifying
-   @Query("UPDATE Users u " +
-           "set u.phoneNumber= :phoneNumber," +
-           "u.vehicleNumber = :vehicleNumber " +
-           "where u.username = :username")
-   int updateDriverDetails(String username, String phoneNumber, @NotBlank String vehicleNumber);
+    @Query("select r.student.username " +
+            "from Rides r where r.id = :rideId")
+    String getUserByRideId(UUID rideId);
 
 
 
